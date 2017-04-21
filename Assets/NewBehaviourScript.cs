6e3a6fb8ec22452a609273c2c55fb8729573d1b6
Rudus <krzysztof.samborowski@gmail.com>
2017-04-21 23:37:24 +0200

    dziala?

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+public string children;
+
+
+    // Use this for initialization
+    void Start() {
+    int i = 0;
+    Debug.Log("Hello my name is " + gameObject + " My parent is: " + GetComponent<Transform>().parent + "My Children are: ");
+    foreach (Transform child in transform)
+        {
+            //children = gameObject;
+            
+            Debug.Log(gameObject.transform.GetChild(i));
+            i++;
+        }
+
+        
+
+        
+        
+            
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
